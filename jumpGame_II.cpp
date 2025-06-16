4bc51afb34f956142b1414a69b07d0162145edf2
Maharshi <joshimaharshi0706@gmail.com>
2025-06-16 18:46:37 +0530

    modified jump game solution wrt greedy approach

--- jumpGame_II.cpp
+++ jumpGame_II.cpp
@@ -6,15 +6,19 @@ using namespace std;
 class Solution {
 public:
   int jump(vector<int>& nums) {
+    if (nums.size() == 1) return 0;
     int currentEnd = 0;
     int nextFurthest = 0;
     int count = 0;
     for(int i = 0; i<=nums.size()-2;i++){
       nextFurthest = max(nextFurthest, i+nums[i]);
+      // cout<<"Next Furthest: "<<nextFurthest<<endl;
       if(i == currentEnd){
         count += 1;
         currentEnd = nextFurthest;
       }
+      // cout<<"Count: "<<count<<endl;
+      // cout<<"Cuurent End: "<<currentEnd<<endl;
       if(currentEnd >= nums.size()-1) break;
     }
     return count;
@@ -24,8 +28,8 @@ public:
 int main(){
   Solution sol;
   // vector<int> nums = {2,3,1,1,4};
-  // vector<int> nums = {2,1};
-  vector<int> nums = {2,1,1,1,1};
+  vector<int> nums = {2};
+  // vector<int> nums = {2,1,1,1,1};
   cout<<sol.jump(nums)<<endl;
   return 0;
 }
