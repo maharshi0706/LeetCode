d7706be5ae04004f36fd39d7fe48e03c8396d760
Maharshi <joshimaharshi0706@gmail.com>
2025-06-19 21:43:15 +0530

    reverse string solution

--- reverseString.cpp
+++ reverseString.cpp
@@ -7,48 +7,41 @@ public:
         string output = "";
         int left = s.length();
         int right = s.length();
-        for(int i = s.length()-1;i >= 0; i--){
+        for(int i = s.length();i >= 0; i--){
             cout<<"CURRENT LETTER IN LOOP: "<<s[i]<<endl;
-            if(s[i] == ' '){
-                if(right <= left) {
-                    cout<<" IN if->if R: "<<s[right]<<" L: "<<s[left]<<endl;
-                    right--;
-                }
-                
-                else{
-                    cout<<" IN if->else R: "<<s[right]<<" L(before): "<<s[left]<<endl;
-                    int temp = -1;
+            if (s[i] != ' '){
+                left = i;
+                cout<<"IN if L: "<<s[left]<<endl;
+            }
+
+            if(s[i] == ' ' || left == 0){               
+                if(left < right){
+                    cout<<" IN if->else R: "<<s[right]<<"POS: "<<right<<endl;
+                    int temp = left;
                     if (s[left] == ' ') temp = left+1;
                     else temp = left;
                     // append word into string
                     while(temp < right){
+                        cout<<"     LETTER: "<<s[temp]<<endl;
                         output += s[temp++];
                     }
                     if(right > left) output += ' ';
-                    right = i;
-                    cout<<" IN if->else R: "<<s[right]<<" L(after): "<<s[left]<<endl;
-                    cout<<" word: "<<output<<endl;
+                    cout<<" word: "<<output<<output.length()<<endl;
                 }
+                right = i;
+                cout<<" IN if->else R: "<<s[right]<<"POS: "<<right<<endl;
             }
             
-            if (s[i] != ' '){
-                left = i;
-                cout<<"IN if L: "<<s[left]<<endl;
-                if(left == 0 && right > 0){
-                    int temp = left;
-                    while(temp < right){
-                        output += s[temp++];
-                    }
-                }
-            }
         }
+        if(output[output.length()-1] == ' ') output = output.erase(output.length()-1);
         return output;
     }
 };
 
 int main(){
     Solution sol;
-    string str = "  hello world  ";
+    string str = "  ";
+    // string str = "  hello world  ";
     // string str = "the sky is blue";
     string output = sol.reverseWords(str);
     cout<<str.length()<<output<<output.length();
