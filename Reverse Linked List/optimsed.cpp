6f0cb6a8600308a09837cb2c5a6c5dd97c352e72
Maharshi <joshimaharshi0706@gmail.com>
2025-08-04 21:42:29 +0530

    Reverse a Singly LL

---
+++ Reverse Linked List/optimsed.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <iostream>
+using namespace std;
+
+class ListNode{
+public:
+    int data;
+    ListNode* next;
+public:
+    ListNode(int data1, ListNode* next1){
+        data = data1;
+        next = next1;
+    }
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp != NULL){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+};
+
+
+void print(ListNode* head){
+    ListNode* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+int main(){
+    Solution sol;
+    ListNode* head = new ListNode(1, nullptr);
+    ListNode* temp = head;
+    for(int i = 0;i < 5;i++){
+        ListNode* newNode = new ListNode(i+5, nullptr);
+        temp->next = newNode;
+        temp = newNode;
+    }
+    print(head);
+    head = sol.reverseList(head);
+    print(head);
+    return 0;
+}
