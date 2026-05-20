936f5bacde0d8b194c355faf08e61863df70bc0e
Maharshi <joshimaharshi0706@gmail.com>
2026-05-20 22:59:39 +0530

    Find Minimum in Rotated Sorted Array - Binary Search Recursive

---
+++ Binary Search/findMinimumInRotatedArray.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution{
+public:
+    int binarySearch(int low, int high, vector<int>& nums, int ans){
+        if(low > high) return ans;
+
+        int mid = low + ((high - low) / 2);
+        // if(nums[mid] < ans) ans = nums[mid];
+
+        // Find sorted half
+        if(nums[low] <= nums[mid]){
+            if(nums[low] < ans) ans = nums[low];
+            return binarySearch(mid + 1, high, nums, ans);
+        }
+        else{
+            if(nums[mid] < ans) ans = nums[mid];
+            return binarySearch(low, mid - 1, nums, ans);
+        }
+    }
+    int findMinimum(vector<int>& nums){
+        int ans = INT32_MAX;
+        ans =  binarySearch(0, nums.size() - 1, nums, ans);
+        return ans;
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> nums = {11,13,15,17};
+    int res = sol.findMinimum(nums);
+    cout<<res<<endl;
+    return 0;
+}
