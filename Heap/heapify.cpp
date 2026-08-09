fae9e8340ed29e54bdf321e84266a82439d609fa
Maharshi <joshimaharshi0706@gmail.com>
2026-08-09 16:56:12 +0530

    Heap Sort - maxHeap

--- Heap/heapify.cpp
+++ Heap/heapify.cpp
@@ -1,22 +1,23 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 class Heap{
 public:
-    void heapifyMax(vector<int>& nums, int i){
+    void heapifyMax(vector<int>& nums, int i, int size){
         int largest = i;
         int leftChildIndex = i*2 + 1;
         int rightChildIndex = i*2 + 2;
 
-        if(leftChildIndex < nums.size() && nums[largest] < nums[leftChildIndex]) largest = leftChildIndex;
+        if(leftChildIndex < size && nums[largest] < nums[leftChildIndex]) largest = leftChildIndex;
 
-        if(rightChildIndex < nums.size() && nums[largest] < nums[rightChildIndex]) largest = rightChildIndex;
+        if(rightChildIndex < size && nums[largest] < nums[rightChildIndex]) largest = rightChildIndex;
 
         if(largest != i){
             std::swap(nums[largest], nums[i]);
             // i = largest;
-            heapifyMax(nums, largest);
+            heapifyMax(nums, largest, size);
         }
     }
     
@@ -38,11 +39,21 @@ public:
 
     void heapify(vector<int>& nums){
         for(int i = nums.size() / 2 - 1;i >= 0;i--){
-            // heapifyMax(nums, i);
-            heapifyMin(nums, i);
+            heapifyMax(nums, i, nums.size());
+            // heapifyMin(nums, i);
         }
 
         // return nums;
+    }
+
+    void heapSort(vector<int>& nums){
+        int size = nums.size();
+        while(size>1){
+            std::swap(nums[0], nums[size-1]);
+            size--;
+    
+            heapifyMax(nums, 0, size);
+        }
     }
 
     void print(vector<int>& nums){
@@ -57,6 +68,8 @@ int main(){
     Heap h;
     vector<int> nums = {54,53,55,52,50};
     h.heapify(nums);
+    h.print(nums);
+    h.heapSort(nums);
     h.print(nums);
     return 0;
 
